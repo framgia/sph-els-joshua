e35c1514d203d1d1776c6d76c704d97ce6ff7cd9
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2022-07-27 11:16:10 +0800

    [FE] User Table Search And Pagination (#6)
    * [FE] User Table Search And Pagination
    
    * Update: Resolve Based on the comments
    
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- frontend/src/pages/admin/users.tsx
+++ frontend/src/pages/admin/users.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { NextPage } from 'next'
+import React, { useState } from 'react'
 
 import { users } from '~/data/userList'
 import Layout from '~/layouts/adminLayout'
@@ -8,12 +8,30 @@ import Pagination from '~/components/admin/Pagination'
 import { adminProtected } from '~/utils/admin-protected'
 
 const Users: NextPage = (): JSX.Element => {
+  const [loading, setLoading] = useState(false)
+
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const userPerPage = 5
+  const pagesVisited = pageNumber * userPerPage
+
+  const displayUsers = users?.slice(pagesVisited, pagesVisited + userPerPage)
+
+  const pageCount = Math.ceil(users?.length / userPerPage)
+
+  const changePage = ({ selected }: { selected: number }): void => setPageNumber(selected)
+
   return (
     <Layout metaTitle="Users">
       <main className="pt-4 px-4">
         <section className="overflow-x-auto relative shadow-md sm:rounded-lg">
-          <UserList users={users} />
-          <Pagination />
+          <UserList users={displayUsers} loading={loading} />
+          <Pagination 
+            length={users?.length}
+            pageNumber={pageNumber}
+            pageCount={pageCount}
+            actions={{ changePage }}
+          />
         </section>
       </main>
     </Layout>
