47025340ef133f56364a636ef48cfa81f628de99
acatzk <joshuaimalay@gmail.com>
2022-07-29 09:56:08 +0800

    Fix: Remove Placehoder Blur

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -21,8 +21,6 @@ const Avatar: React.FC<Props> = (props): JSX.Element => {
           alt="avatar" 
           className="rounded-full"
           layout="intrinsic"
-          placeholder="blur"
-          blurDataURL={url}
         />
       </div>
     </div>
