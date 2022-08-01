8c0bec56f486f68d1e2e8b53ffe9d6aebcbef0e2
acatzk <joshuaimalay@gmail.com>
2022-08-01 11:47:36 +0800

    [FE/BE] Display Category Data Functionality

--- frontend/src/pages/admin/categories/index.tsx
+++ frontend/src/pages/admin/categories/index.tsx
@@ -1,23 +1,32 @@
+import useSWR from 'swr'
 import { NextPage } from 'next'
+import { AxiosResponse } from 'axios'
 import React, { useState } from 'react'
 
+import axios from '~/lib/axios'
 import Layout from '~/layouts/adminLayout'
-import { categories } from '~/data/categories'
 import Pagination from '~/components/admin/Pagination'
 import { adminProtected } from '~/utils/admin-protected'
 import CategoryList from '~/components/admin/CategoryList'
 
+const fetcher = (url: string) => axios.get(url).then((res: AxiosResponse) => res.data)
+
 const Categories: NextPage = (): JSX.Element => {
-  const [loading, setLoading] = useState(false)
+  const { data: categories } = useSWR('/api/categories', async () => fetcher('/api/categories'), {
+    refreshInterval: 1000,
+    revalidateOnMount: true
+  })
+
+  const loading = !categories?.data
 
   const [pageNumber, setPageNumber] = useState(0)
 
   const cagoryPerPage = 5
   const pagesVisited = pageNumber * cagoryPerPage
 
-  const displayCategories = categories?.slice(pagesVisited, pagesVisited + cagoryPerPage)
+  const displayCategories = categories?.data?.slice(pagesVisited, pagesVisited + cagoryPerPage)
 
-  const pageCount = Math.ceil(categories?.length / cagoryPerPage)
+  const pageCount = Math.ceil(categories?.data?.length / cagoryPerPage)
 
   const changePage = ({ selected }: { selected: number }): void => setPageNumber(selected)
 
@@ -26,12 +35,14 @@ const Categories: NextPage = (): JSX.Element => {
       <main className="pt-4 px-4">
         <section className="overflow-x-auto relative shadow-md sm:rounded-lg">
           <CategoryList categories={displayCategories} loading={loading} />
-          <Pagination 
-            length={categories?.length}
-            pageNumber={pageNumber}
-            pageCount={pageCount}
-            actions={{ changePage }}
-          />
+          {!loading && (
+            <Pagination 
+              length={categories?.length}
+              pageNumber={pageNumber}
+              pageCount={pageCount}
+              actions={{ changePage }}
+            />
+          )}
         </section>
       </main>
     </Layout>
