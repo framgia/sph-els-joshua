ed9b068d5caf658ad0ad49f66b41d0a06b3689a1
acatzk <joshuaimalay@gmail.com>
2022-08-03 19:06:46 +0800

    [FE] User Categories Page

---
+++ frontend/src/pages/categories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '~/layouts/userLayout'
+import CategoryCard from '~/components/user/CategoryCard'
+
+const Categories = (): JSX.Element => {
+  return (
+    <Layout metaTitle="Categories">
+      <div className="pt-5 space-x-4 overflow-hidden">
+        <div className="grid grid-cols-3 gap-4">
+          {[0,1,2,3,4,5].map((i: number) => <CategoryCard key={i} />)}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Categories
