22095255226c92692623e82f6d7ff1692519607e
acatzk <joshuaimalay@gmail.com>
2022-08-04 16:21:14 +0800

    [FE] Answer Quiz Page

--- frontend/src/components/user/CategoryCard.tsx
+++ frontend/src/components/user/CategoryCard.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
-const CategoryCard = (): JSX.Element => {
+import LessonConfirmationDialog from './LessonConfirmationDialog'
+
+type Props = {
+  i: number
+}
+
+const CategoryCard: React.FC<Props> = ({ i }): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const closeModal = () => setIsOpen(false)
+
+  const openModal = () => setIsOpen(true)
+
   return (
     <div className="bg-white rounded-lg shadow-md border">
       <div className="px-5 py-5">
@@ -9,7 +22,17 @@ const CategoryCard = (): JSX.Element => {
             <p className="text-sm line-clamp-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus possimus ab cum ipsam non eum perferendis adipisci qui impedit, modi quaerat sed. Necessitatibus nostrum quo sit provident esse sapiente perspiciatis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea culpa at cum nisi quod est odio voluptate quo, possimus non quas nostrum quibusdam! Odio blanditiis, architecto magni ea itaque totam.</p>
           </div>
           <div className="mt-3 flex justify-end items-center">
-            <button type="button" className="btn-primary">Start</button>
+            <button 
+              type="button"
+              onClick={openModal}
+            >
+              <a className="btn-primary">Start</a>
+            </button>
+            <LessonConfirmationDialog 
+              isOpen={isOpen}
+              closeModal={closeModal}
+              categories={[i]}
+            />
           </div>
       </div>
     </div>
--- frontend/src/pages/categories/index.tsx
+++ frontend/src/pages/categories/index.tsx
@@ -8,7 +8,7 @@ const Categories = (): JSX.Element => {
     <Layout metaTitle="Categories">
       <div className="pt-5 space-x-4 overflow-hidden">
         <div className="grid grid-cols-3 gap-4">
-          {[0,1,2,3,4,5].map((i: number) => <CategoryCard key={i} />)}
+          {[0,1,2,3,4,5].map((i: number) => <CategoryCard key={i} i={i} />)}
         </div>
       </div>
     </Layout>
