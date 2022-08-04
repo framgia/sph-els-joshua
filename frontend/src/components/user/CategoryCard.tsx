81265f36772434c974668a22b3d1460d4ccadff0
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2022-08-04 16:11:01 +0800

    [FE] User Settings Page (#25)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- frontend/src/components/user/CategoryCard.tsx
+++ frontend/src/components/user/CategoryCard.tsx
@@ -9,7 +9,7 @@ const CategoryCard = (): JSX.Element => {
             <p className="text-sm line-clamp-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus possimus ab cum ipsam non eum perferendis adipisci qui impedit, modi quaerat sed. Necessitatibus nostrum quo sit provident esse sapiente perspiciatis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea culpa at cum nisi quod est odio voluptate quo, possimus non quas nostrum quibusdam! Odio blanditiis, architecto magni ea itaque totam.</p>
           </div>
           <div className="mt-3 flex justify-end items-center">
-            <button type="button" className="btn-success">Start</button>
+            <button type="button" className="btn-primary">Start</button>
           </div>
       </div>
     </div>
